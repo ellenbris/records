c0906eb94ddd5f4290257e961c40c0aa66649104
Ellen Brisley <ellenbrisley@gmail.com>
2024-02-16 10:56:43 -0800

    view records

--- medicalrecords/views.py
+++ medicalrecords/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Records
 
 # Create your views here.
 
 # Render page 
 def index(request): 
-    return render(request, 'index.html')
+    record = Records.objects.all()
+
+    # save new data
+    if  request.method == 'POST': 
+        new_record = Records (
+            title = request.POST['title']
+        )
+        new_record.save()
+        return redirect('/')
+
+    return render(request, 'index.html', {'records':record})
