e90e0cd5dabef98a39d6927de8a4639de796723c
Ellen Brisley <ellenbrisley@gmail.com>
2024-02-16 10:35:27 -0800

    models setup

--- medicalrecords/models.py
+++ medicalrecords/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# Takes only one field 
+class Records(models.Model):
+    title = models.CharField(max_length = 1000)
+
+    def __str__(self): 
+        return self.title
